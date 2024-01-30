44dc1dd70e9c337565837675af6a78522e20de9b
Satoru Ogura <sat@st.rim.or.jp>
2024-01-31 03:54:19 +0900

    WIP

--- jp-object.js
+++ jp-object.js
@@ -1,9 +1,19 @@
 const
-AE = ( $, _ ) => $.appendChild( document.createElement( _ ) )
-const
-AT = ( $, _ ) => $.appendChild( document.createTextNode( _ ) )
+CE = _ => document.createElement( _ )
 
-export class
+const
+CT = _ => document.createTextNode( _ )
+
+const
+AC = ( $, _ ) => $.appendChild( _ )
+
+const
+AE = ( $, _ ) => AC( $, CE( _ ) )
+
+const
+AT = ( $, _ ) => AC( $, CT( _ ) )
+
+export default class
 JPObject extends HTMLElement {
 
 	constructor( $, ParentAndKey ) {
@@ -11,34 +21,47 @@ JPObject extends HTMLElement {
 
 		this.$ = $
 		this.ParentAndKey = ParentAndKey
-		this.classList.add( `jp-object` )
-	}
-
-	attributeChangedCallback( name, oldValue, newValue ) {
-		name === 'json' && (
-			this.$ = JSON.parse( newValue )
-		,	this.Refresh()
-		)
 	}
 
 	connectedCallback() {
-		this.getAttribute( 'json' ) != null && ( this.$ = JSON.parse( this.getAttribute( 'json' ) ) )
-		this.Refresh()
+		const	json = this.getAttribute( 'json' )
+//		console.log( 'CCB', this.$, json )
+		json && ( this.$ = JSON.parse( json ) )
+		this.Reconstruct()
 	}
 
-	//	Virtual
-	NewJPObject( v, k ) {
-		return new JPObject( v, [ this, k ] )
+	attributeChangedCallback( name, oldValue, newValue ) {
+//		console.log( 'ACC', name, newValue )
+		name === 'json' && ( this.$ = JSON.parse( newValue ) )
+		this.Reconstruct()
 	}
-	//	Virtual
-	ConfigKeySpan( span, k ) {
+
+	Top() {
+		return this.ParentAndKey ? this.ParentAndKey[ 0 ].Top() : this
 	}
-	ObjectOrArray( openP, closeP ) {
-		this.textContent = ''
+
+	Editable() {
+		return this.Top().getAttribute( 'editable'		) === ''
+	}
+	Appendable() {
+		return this.Top().getAttribute( 'appendable'	) === ''
+	}
+	Removable() {
+		return this.Top().getAttribute( 'removable'		) === ''
+	}
+
+	Broadcast() {
+		this.parentAndKey
+		?	this.ParentAndKey[ 0 ].Broadcast()
+		:	this.Broadcaster( this )
+	}
+
+	ObjectOrArray( openP, closeP, remover, appender, keyElement ) {
+		this.innerHTML = ''
 		const anchor = AE( this, 'a' )
 		anchor.onclick = () => (
 			this.toggleAttribute( 'open' )
-		,	this.ObjectOrArray( openP, closeP )
+		,	this.ObjectOrArray( openP, closeP, remover, appender )
 		)
 		anchor.textContent = Object.keys( this.$ ).length
 		if ( this.getAttribute( 'open' ) != null ) {
@@ -47,55 +70,145 @@ JPObject extends HTMLElement {
 				( [ k, v ] ) => {
 					const div = AE( this, 'div' )
 					const span = AE( div, 'span' )
+					span.classList.add( 'jp-object-key' )
 					span.textContent = k
-					this.ConfigKeySpan( span, k )
-					div.appendChild( this.NewJPObject( v, k ) )
+					this.Removable() && (
+						span.onclick = () => {
+							remover( k )
+							this.Reconstruct()
+						}
+					)
+					AC( div, new JPObject( v, [ this, k ] ) )
 				}
 			)
 			AT( this, closeP )
 		} else {
 			AT( this, openP + '...' + closeP )
 		}
+		if ( this.Appendable() ) {
+			if ( keyElement ) AC( this, keyElement )
+			const select	= AE( this, 'select' )
+			const AddOption	= _ => AE( select, 'option' ).textContent = _
+			AddOption( 'String' )
+			AddOption( 'Number' )
+			AddOption( 'Boolean' )
+			AddOption( 'Object' )
+			AddOption( 'Array' )
+			AddOption( 'null' )
+			const addB = AE( this, 'span' )
+			addB.textContent = '+'
+			addB.onclick = () => {
+				switch ( select.value ) {
+				case 'String'	: appender( '' )	; break
+				case 'Number'	: appender( 0 )		; break
+				case 'Boolean'	: appender( false )	; break
+				case 'Object'	: appender( {} )	; break
+				case 'Array'	: appender( [] )	; break
+				case 'null'		: appender( null )	; break
+				}
+				this.Reconstruct()
+				this.Broadcast()
+			}
+		}
 	}
 
-	//	Virtual
-	ObjectElement() {
-		this.ObjectOrArray( '{', '}' )
-	}
-	//	Virtual
-	ArrayElement() {
-		this.ObjectOrArray( '[', ']' )
-	}
-	//	Virtual
-	StringElement() {
-		this.textContent = this.$
-	}
-	//	Virtual
-	NumberElement() {
-		this.textContent = this.$
-	}
-	//	Virtual
-	BooleanElement() {
-		this.textContent = this.$
-	}
-
-	Refresh() {
+	Reconstruct() {
 		switch ( this.$ ) {
 		case void 0:
 			this.classList.add( `jp-object-undefined` )
 			break
 		case null:
-			this.textContent = 'null'
 			this.classList.add( `jp-object-null` )
+			this.textContent = 'null'
 			break
 		default:
 			this.classList.add( 'jp-object-' + this.$.constructor.name )
 			switch ( this.$.constructor ) {
-			case Object	: this.ObjectElement()	; break
-			case Array	: this.ArrayElement()	; break
-			case String	: this.StringElement()	; break
-			case Number	: this.NumberElement()	; break
-			case Boolean: this.BooleanElement()	; break
+			case Object	:
+				{	const keyElement = CE( 'input' )
+					keyElement.classList.add( 'jp-object-key-name' )
+					keyElement.setAttribute( 'placeholder', 'Key:' )
+					this.ObjectOrArray(
+						'{'
+					,	'}'
+					,	_ => delete this.$[ _ ]
+					,	_ => {
+							const key = keyElement.value
+							this.$[ key ] = _
+						}
+					,	keyElement
+					)
+				}
+				break;
+			case Array	:
+				this.ObjectOrArray(
+					'['
+				,	']'
+				,	_ => this.$.splice( _, 1 )
+				,	_ => this.$.push( _ )
+				)
+				break;
+			case String	:
+				if ( this.Editable() ) {
+					const input = AE( this, 'input' )
+					input.value = this.$
+					input.onchange = () => {
+						if ( this.ParentAndKey === void 0 ) {
+							this.$ = input.value
+						} else {
+							const [ parent, key ] = this.ParentAndKey
+							parent.$[ key ] = input.value
+							parent.Reconstruct()
+						}
+						this.Broadcast()
+					}
+				} else {
+					this.textContent = this.$
+				}
+				break;
+			case Number	:
+				if ( this.Editable() ) {
+					const input = AE( this, 'input' )
+					input.value = this.$
+					input.onchange = () => {
+						const _ = Number( input.value )
+						if ( isNaN( _ ) ) {
+							alert( '"' + input.value + '" is not a number.' )
+							input.value = this.$
+							return
+						}
+						if ( this.ParentAndKey === void 0 ) {
+							this.$ = _
+						} else {
+							const [ parent, key ] = this.ParentAndKey
+							parent.$[ key ] = input.value
+							parent.Reconstruct()
+						}
+						this.Broadcast()
+					}
+				} else {
+					this.textContent = this.$
+				}
+				break;
+			case Boolean:
+				if ( this.Editable() ) {
+					const input = AE( this, 'input' )
+					input.setAttribute( 'type', 'checkbox' )
+					input.checked = this.$
+					input.onchange = () => {
+						if ( this.ParentAndKey === void 0 ) {
+							this.$ = input.checked
+						} else {
+							const [ parent, key ] = this.ParentAndKey
+							parent.$[ key ] = input.checked
+							parent.Reconstruct()
+						}
+						this.Broadcast()
+					}
+				} else {
+					this.textContent = this.$
+				}
+				break;
 			default		:
 				this.textContent = this.$
 				break
@@ -105,73 +218,4 @@ JPObject extends HTMLElement {
 	}
 }
 
-export class
-JPObjectEditor extends JPObject {
-
-	NewJPObject( v, k ) {
-		return new JPObjectEditor( v, [ this, k ] )
-	}
-	ConfigKeySpan( span, k ) {
-		span.onclick = () => (
-			this.eraser( k )
-		,	this.Refresh()
-		)
-	}
-	ObjectElement() {
-		this.eraser = _ => delete this.$[ _ ]
-		super.ObjectElement()
-	}
-	ArrayElement() {
-		this.eraser = _ => this.$.splice( _, 1 )
-		super.ArrayElement()
-	}
-	StringElement() {
-		const input = AE( this, 'input' )
-		input.value = this.$
-		input.onchange = () => {
-			if ( this.ParentAndKey === void 0 ) {
-				this.$ = input.value
-			} else {
-				const [ parent, key ] = this.ParentAndKey
-				parent.$[ key ] = input.value
-				parent.Refresh()
-			}
-		}
-	}
-	NumberElement() {
-		const input = AE( this, 'input' )
-		input.value = this.$
-		input.onchange = () => {
-			const _ = Number( input.value )
-			if ( isNaN( _ ) ) {
-				alert( '"' + input.value + '" is not a number.' )
-				input.value = this.$
-				return
-			}
-			if ( this.ParentAndKey === void 0 ) {
-				this.$ = _
-			} else {
-				const [ parent, key ] = this.ParentAndKey
-				parent.$[ key ] = input.value
-				parent.Refresh()
-			}
-		}
-	}
-	BooleanElement() {
-		const input = AE( this, 'input' )
-		input.setAttribute( 'type', 'checkbox' )
-		input.checked = this.$
-		input.onchange = () => {
-			if ( this.ParentAndKey === void 0 ) {
-				this.$ = input.checked
-			} else {
-				const [ parent, key ] = this.ParentAndKey
-				parent.$[ key ] = input.checked
-				parent.Refresh()
-			}
-		}
-	}
-}
-
 customElements.define( 'jp-object', JPObject )
-customElements.define( 'jp-object-editor', JPObjectEditor )
